16ae1084e42c910114d32ab5b1ae6fd54fa6cd8f
HenryHYH <henry.hyh@gmail.com>
2015-07-01 09:37:26 +0800

    Finish solution

---
+++ ExcelSheetColumnNumber/ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rowNum = "AA";
+            var solution = new Solution();
+
+            Console.WriteLine("{0} = {1}", rowNum, solution.TitleToNumber(rowNum));
+        }
+
+        private readonly Solution solution;
+
+        public Program()
+        {
+            solution = new Solution();
+        }
+
+        [Theory]
+        [InlineData("A", 1)]
+        [InlineData("Z", 26)]
+        [InlineData("AA", 27)]
+        [InlineData("AB", 28)]
+        [InlineData("BA", 53)]
+        public void Test(string rowNum, int expected)
+        {
+            Assert.Equal(expected, solution.TitleToNumber(rowNum));
+        }
+    }
+}
---
+++ ExcelSheetColumnNumber/ConsoleApp/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Solution
+    {
+        public int TitleToNumber(string s)
+        {
+            var num = 0;
+            for (int i = 0, iMax = s.Length; i < iMax; i++)
+            {
+                num = num * 26 + s[i] - 'A' + 1;
+            }
+
+            return num;
+        }
+    }
+}
