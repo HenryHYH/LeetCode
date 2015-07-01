6545312d0fbfbf61e237edc4b02ed6adfd47e957
HenryHYH <henry.hyh@gmail.com>
2015-07-01 21:15:38 +0800

    Finish Happy Number

---
+++ HappyNumber/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private readonly Solution solution;
+
+        public Program()
+        {
+            solution = new Solution();
+        }
+
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(2, false)]
+        [InlineData(19, true)]
+        [InlineData(18, false)]
+        public void Test(int number, bool expected)
+        {
+            Assert.Equal(expected, solution.IsHappy(number));
+        }
+    }
+}
---
+++ HappyNumber/ConsoleApp/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Solution
+    {
+        public bool IsHappy(int n)
+        {
+            if (1 == n)
+                return true;
+            else if (4 == n)
+                return false;
+            else
+            {
+                int tmp = 0;
+                while (n > 0)
+                {
+                    tmp += (int)Math.Pow(n % 10, 2);
+                    n = (n - n % 10) / 10;
+                }
+
+                return IsHappy(tmp);
+            }
+        }
+    }
+}
