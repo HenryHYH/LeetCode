583a15a0a2f2326af85e9503cb2b08a4631bed64
HenryHYH <henry.hyh@gmail.com>
2015-11-18 15:58:57 +0800

    Missing number

---
+++ MissingNumber/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [Theory]
+        [InlineData(new[] { 0, 1, 3 }, 2)]
+        [InlineData(new[] { 0 }, 1)]
+        [InlineData(new[] { 1 }, 0)]
+        [InlineData(new[] { 0, 1 }, 2)]
+        [InlineData(new[] { 1, 0 }, 2)]
+        [InlineData(new[] { 1, 2 }, 0)]
+        public void Test(int[] arr, int expected)
+        {
+            int actual = new Solution().MissingNumber(arr);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ MissingNumber/ConsoleApp/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            var len = nums.Length;
+            if (1 == len)
+                return 0 == nums[0] ? 1 : (nums[0] - 1);
+
+            nums = nums.OrderBy(x => x).ToArray();
+            int last = nums[0];
+
+            for (int i = 1; i < len; i++)
+            {
+                if (nums[i] - last > 1)
+                    return nums[i] - 1;
+
+                last = nums[i];
+            }
+
+            return 0 == nums[0] ? last + 1 : 0;
+        }
+    }
+}
