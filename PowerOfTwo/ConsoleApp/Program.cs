ae5915d60512093ab4e4c4c86af81a4628f9d567
HenryHYH <henry.hyh@gmail.com>
2015-11-18 16:08:39 +0800

    PowerOfTwo

---
+++ PowerOfTwo/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(1, true)]
+        [InlineData(2, true)]
+        [InlineData(127, false)]
+        [InlineData(128, true)]
+        public void Test(int n, bool expected)
+        {
+            var actual = new Solution().IsPowerOfTwo(n);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ PowerOfTwo/ConsoleApp/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (0 == n)
+                return false;
+            else if (n == 1 || n == 2)
+                return true;
+
+            return n % 2 == 0 && IsPowerOfTwo(n / 2);
+        }
+    }
+}
