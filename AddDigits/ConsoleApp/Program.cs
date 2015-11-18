cac5748cd09efdd73a4a96d2dff01b8a5eb4d856
HenryHYH <henry.hyh@gmail.com>
2015-11-18 14:21:44 +0800

    Add digits

---
+++ AddDigits/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [Theory]
+        [InlineData(38, 2)]
+        public void Test(int num, int expected)
+        {
+            var actual = new Solution().AddDigits(num);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AddDigits/ConsoleApp/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Solution
+    {
+        public int AddDigits(int num)
+        {
+            if (num < 10)
+                return num;
+
+            int tmp = num;
+            num = 0;
+
+            while (tmp >= 10)
+            {
+                num += tmp % 10;
+                tmp = tmp / 10;
+            }
+            num += tmp;
+
+            return AddDigits(num);
+        }
+    }
+}
