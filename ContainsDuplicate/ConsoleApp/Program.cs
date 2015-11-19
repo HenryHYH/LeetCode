f2a4f38006b2938f8397afacd03430301e16f40b
HenryHYH <henry.hyh@gmail.com>
2015-11-19 14:04:15 +0800

    Contains duplicate

---
+++ ContainsDuplicate/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [Theory]
+        [InlineData(new int[] { }, false)]
+        [InlineData(new[] { 0 }, false)]
+        [InlineData(new[] { 0, 1 }, false)]
+        [InlineData(new[] { 0, 1, 1 }, true)]
+        public void Test(int[] nums, bool expected)
+        {
+            var actual = new Solution().ContainsDuplicate(nums);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ ContainsDuplicate/ConsoleApp/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            if (0 == nums.Length)
+                return false;
+
+            var hs = new HashSet<int>() { nums[0] };
+            for (int i = 1, iMax = nums.Length; i < iMax; i++)
+            {
+                if (hs.Contains(nums[i]))
+                    return true;
+
+                hs.Add(nums[i]);
+            }
+
+            return false;
+        }
+    }
+}
